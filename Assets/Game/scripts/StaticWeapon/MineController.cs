51bf86de897b93f6737d2900a747fe223fec131e
NikitaD <zoomguru1@gmail.com>
2025-04-24 23:00:39 +0300

    [+] add mine , mine controller

---
+++ Assets/Game/scripts/StaticWeapon/MineController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MineController : MonoBehaviour
+{
+    [SerializeField] private Mine minePrefab;
+    [SerializeField] private float coolDown;
+    [SerializeField] private RandomPointInBoxCollider randomPointInBoxCollider;
+    private float timer;
+    
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= coolDown)
+        {
+            timer = 0f;
+            Instantiate(minePrefab, randomPointInBoxCollider.GetRandomPointInBox(), Quaternion.identity);
+        }
+    }
+}
