8357c51ca2983af4f513460a76e6b75d00068357
ololoLLl228 <60641363+ololoLLl228@users.noreply.github.com>
2025-05-01 10:18:05 +0300

    [+]addedLaser

--- Assets/Game/scripts/Bullet/LaserRay.cs
+++ Assets/Game/scripts/Bullet/LaserRay.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
-public class LaserRay : MonoBehaviour
+public class LaserRay : MonoBehaviour, ITickable
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Camera _playerCamera;
+    [SerializeField] private Transform _laserOrigin;
+    [SerializeField] private float _gunRange;
+    [SerializeField] private float _laserDuration;
+    [SerializeField] private LineRenderer _laserLine;
+    [SerializeField] private float _shootingInterval;
+    [SerializeField] private float _shootingTimer;
+    [Inject] GameController gameController;
+    private void Awake()
     {
-        
+        gameController.RegisterInTick(this);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Tick()
     {
-        
+        _shootingTimer += Time.deltaTime;
+        if(_shootingTimer >= _shootingInterval)
+        {
+            _shootingTimer = 0;
+            _laserLine.SetPosition(0, _laserOrigin.position);
+            Vector3 rayOrigin = _playerCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+            if(Physics.Raycast(rayOrigin, _playerCamera.transform.forward, out hit, _gunRange))
+            {
+                _laserLine.SetPosition(1, hit.point);
+                Debug.Log("laser hit " + hit.transform.name);
+            }
+            else
+            {
+                _laserLine.SetPosition(1, rayOrigin + (_playerCamera.transform.forward * _gunRange));
+            }
+        }
     }
 }
