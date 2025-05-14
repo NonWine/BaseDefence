f1cb2a40a85ff07febf6dbd557e39066b1640f72
ololoLLl228 <60641363+ololoLLl228@users.noreply.github.com>
2025-05-15 00:06:37 +0300

    [+] added bigZombie

---
+++ Assets/Game/scripts/Enemy/Core/BigZombie.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BigZombie : BaseEnemy
+{
+    public override Type Type => typeof(BigZombie);
+}
