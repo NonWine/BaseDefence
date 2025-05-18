8afff3a4b84d8f4151d2945376c395d88c4aee66
NikitaD <zoomguru1@gmail.com>
2025-05-18 13:11:18 +0300

    [+] ammo box

---
+++ Assets/Game/scripts/StaticWeapon/BoxAmmoController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class BoxAmmoController : StaticWeaponController
+{
+    [SerializeField] private Transform box;
+    
+    private void Start()
+    {
+        UnlockCallback += CreateTower;
+    }
+
+    private void CreateTower()
+    {
+        box.gameObject.SetActive(true);        
+    }
+    protected override void UnLockedUpdate()
+    {
+
+    }
+}
